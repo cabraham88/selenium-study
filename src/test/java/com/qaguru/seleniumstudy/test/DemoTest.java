bd2bda1aa176efefddfd4f2f4c44dcf37f5d915c
BasilAnna <chinnuannaabrham@gmail.com>
2022-03-15 17:05:39 -0400

    Added test for searching a product.Closes #2

--- src/test/java/com/qaguru/seleniumstudy/test/DemoTest.java
+++ src/test/java/com/qaguru/seleniumstudy/test/DemoTest.java
@@ -2,7 +2,9 @@ package com.qaguru.seleniumstudy.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,7 +19,7 @@ public class DemoTest {
         WebDriverManager.chromedriver().setup();//launch the browser.driver to chrome, translate to chrome
 //safari dont need to setup,it comes automatically
         //WebDriverManager is class, chromedriver is static method,setup() is method
-        WebDriver driver=new ChromeDriver();//selenium
+        WebDriver driver=new ChromeDriver();//selenium webdriver class type instance
         driver.get(baseUrl);// navigate to the browser
         String expTitle="Furniture, Kitchen, Dining Room, Entertainment, Bedroom Sets, Outdoor, Fireplaces";
         String actTitle= driver.getTitle();
@@ -26,5 +28,20 @@ public class DemoTest {
         driver.quit();//quit release the entire instance of that driver, .close() is to close the window
 
     }
+    @Test
+    public void testSearchingForAProduct() {
 
-}
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+        WebElement txtSearch = driver.findElement(By.name("search_field"));//created an object and finding the testbox
+        txtSearch.sendKeys("table");//code for typing text table on the testbox
+        WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"search\"]/form/div/span/input"));// finding the GO button
+        btnSearch.click();//click the go button
+        WebElement lnkResult = driver.findElement(By.linkText("Desk-Standing Converter Large"));
+        driver.quit();
+    }
+
+    }
+
+
